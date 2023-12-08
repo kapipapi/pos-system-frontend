573b911847df64c325b6cce6883cd91ee5b2f264
Kacper Ledwosiński <kl@airotec.pl>
2023-12-08 14:33:55 +0100

    add product form

--- src/app/settings/components/new-product-form.tsx
+++ src/app/settings/components/new-product-form.tsx
@@ -1,4 +1,4 @@
-import {FC} from "react";
+import {FC, useEffect} from "react";
 import ReactModal from "react-modal";
 import {NewProduct} from "../../../models/product";
 import {useForm} from "react-hook-form";
@@ -9,49 +9,99 @@ type Props = {
     onSubmit: (newProduct: NewProduct) => void;
 };
 const NewProductForm: FC<Props> = ({modalState, closeModal, onSubmit}) => {
-    const {register, handleSubmit, watch, formState: {errors}} = useForm<NewProduct>();
+    const {register, handleSubmit, reset, formState: {errors}} = useForm<NewProduct>();
+
+    useEffect(() => {
+        console.log(errors)
+    }, [errors]);
 
     return (
         <ReactModal
             isOpen={modalState}
+            ariaHideApp={false}
             contentLabel="Minimal Modal Example"
+            onRequestClose={() => {
+                closeModal();
+                reset();
+            }}
+            style={{
+                content: {
+                    margin: 'auto auto',
+                    width: '50%',
+                }
+            }}
         >
-            <div>
+            <div className={"mx-auto w-1/2"}>
+                <h1 className={"font-bold text-2xl text-center"}>Create new product</h1>
                 <form
                     onSubmit={handleSubmit((data) => {
                         onSubmit(data);
                         closeModal();
+                        reset();
                     })}
                     className={"flex flex-col space-y-2"}
                 >
-                    <label>Name</label>
-                    <input {...register("name")} className={"border"}/>
+                    <div>
+                        <label>Name</label>
+                        <br/>
+                        <input {...register("name", {required: "Name is required"})} className={"border"}/>
+                        <p className={"text-sm text-red-600"}>{errors.name?.message}</p>
+                    </div>
 
-                    <label>Price</label>
-                    <input type={"number"} {...register("price", {
-                        valueAsNumber: true,
-                        validate: (value) => value > 0,
-                    })} className={"border"}/>
+                    <div>
+                        <label>Price</label>
+                        <br/>
+                        <input type={"number"} {...register("price", {
+                            required: "Price is required",
+                            valueAsNumber: true,
+                            validate: {
+                                positive: v => v > 0 || 'Price must be positive',
+                            },
+                        })} className={"border"}/>
+                        <p className={"text-sm text-red-600"}>{errors.price?.message}</p>
+                    </div>
 
-                    <label>Tax</label>
-                    <input type={"number"} {...register("tax", {
-                        valueAsNumber: true,
-                        validate: (value) => value > 0,
-                    })} className={"border"}/>
+                    <div>
+                        <label>Tax</label>
+                        <br/>
+                        <input type={"number"} {...register("tax", {
+                            required: "Tax is required",
+                            valueAsNumber: true,
+                            validate: {
+                                positive: v => v > 0 || 'Tax must be positive',
+                            },
+                        })} className={"border"}/>
+                        <p className={"text-sm text-red-600"}>{errors.tax?.message}</p>
+                    </div>
 
-                    <label>Category</label>
-                    <input {...register("category")} className={"border"}/>
+                    <div>
+                        <label>Category</label>
+                        <br/>
+                        <input {...register("category", {required: "Category is required"})} className={"border"}/>
+                        <p className={"text-sm text-red-600"}>{errors.category?.message}</p>
+                    </div>
 
-                    <label>Description</label>
-                    <input {...register("description")} className={"border"}/>
+                    <div>
+                        <label>Description</label>
+                        <br/>
+                        <input {...register("description", {required: "Description is required"})}
+                               className={"border"}/>
+                        <p className={"text-sm text-red-600"}>{errors.description?.message}</p>
+                    </div>
 
-                    <button type="submit" className={"inline-flex items-center border rounded-md p-1"}>
-                        Create New Order
-                    </button>
+                    <div className={"w-full space-x-4"}>
+                        <button onClick={() => {
+                            closeModal();
+                            reset();
+                        }} className={"inline-flex items-center border rounded-md p-1"}>
+                            Close Modal
+                        </button>
+                        <button type="submit" className={"inline-flex items-center border rounded-md p-1"}>
+                            Create New Order
+                        </button>
+                    </div>
                 </form>
-                <button onClick={() => closeModal()} className={"inline-flex items-center border rounded-md p-1"}>
-                    Close Modal
-                </button>
+
             </div>
         </ReactModal>
     );
